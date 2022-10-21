fee258599c68bd21ccdfe78aaec8a6b9ac919ba6
shad mustafa <shadmanmi2018@gmail.com>
2022-10-21 20:36:28 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
+// // Write your solution in this file!
+// var customerName='bob';
+
+
+
+// function upperCaseCustomerName(){
+
+// customerName.toUpperCase();
+  
+
+// }
+// upperCaseCustomerName();
+
+
+
+// function setBestCustomer(){
+// var bestCustomer='not bob';
+// }
+
+// setBestCustomer( bestCustomer);
+
+// function overwriteBestCustomer(estCustomer,customerName){
+//    return bestCustomer='maybe bob';
+// }
+
+
+
+// overwriteBestCustomer(bestCustomer, customerName);
+
+// const leastFavoriteCustomer='';
+
+// function changeLeastFavoriteCustomer(){
+//     return leastFavoriteCustomer='shad';
+// }
+
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+var bestCustomer;
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+   return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+    return bestCustomer;
+}
+const leastFavoriteCustomer="";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="bob";
+    return leastFavoriteCustomer;
+}
